f69d6a1ee2dd7ebef4244986e8b22b8f1092eb57
Sazzad <sazzad@hsenidmobile.com>
2020-05-03 16:27:01 +0600

    clean up

--- src/main/java/com/lynas/Application.java
+++ src/main/java/com/lynas/Application.java
@@ -8,12 +8,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.Arrays;
 import java.util.UUID;
 
-@EnableTransactionManagement
 @SpringBootApplication
 public class Application {
     @Bean
